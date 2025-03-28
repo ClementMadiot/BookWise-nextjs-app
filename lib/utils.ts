7f84104c04ce3dd727536dccfc691d75a2daf626
ClementMadiot <clementmadiot09@gmail.com>
2025-03-28 15:05:18 +0100

    profile page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Get Initials user name -> Avatar shadcdn
+export const getInitials = (name: string): string =>
+  name
+    .split(" ")
+    .map((n) => n[0])
+    .join("")
+    .toUpperCase()
+    .slice(0, 2);
