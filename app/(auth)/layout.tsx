ecec23e92957a62515c7d3d6f0967ebcf3a61340
ClementMadiot <clementmadiot09@gmail.com>
2025-03-28 11:45:50 +0100

    redirect if login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,25 +1,37 @@
+import { auth } from "@/auth";
+import { redirect } from 'next/navigation';
 import Image from "next/image";
 import { ReactNode } from "react";
 
-const layout = ({ children }: { children: ReactNode }) => {
-  return <main className="auth-container">
-    <section className="auth-form">
-      <div className="auth-box">
-        {/* logo  */}
-        <div className="flex flex-row gap-2">
-          <Image src={'/icons/logo.svg'} width={37} height={37} alt="logo" />
-          <h1 className="text-2xl font-semibold text-white">BookWise</h1>
-        </div>
-        {/* section children  */}
-        <div>{children}</div>
-        
-      </div>
-    </section>
+const layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
 
-    <section className="auth-illustration">
-      <Image src={'/images/auth-illustration.png'} width={1000} height={1000} alt="auth illustration" className="size-full object-cover" />
-    </section>
-  </main>;
+  if (session) redirect("/");
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          {/* logo  */}
+          <div className="flex flex-row gap-2">
+            <Image src={"/icons/logo.svg"} width={37} height={37} alt="logo" />
+            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+          </div>
+          {/* section children  */}
+          <div>{children}</div>
+        </div>
+      </section>
+
+      <section className="auth-illustration">
+        <Image
+          src={"/images/auth-illustration.png"}
+          width={1000}
+          height={1000}
+          alt="auth illustration"
+          className="size-full object-cover"
+        />
+      </section>
+    </main>
+  );
 };
 
 export default layout;
