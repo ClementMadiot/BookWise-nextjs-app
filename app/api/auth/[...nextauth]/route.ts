51be6090c5014ab27b558d943344b78415cc2275
ClementMadiot <clementmadiot09@gmail.com>
2025-03-28 10:51:18 +0100

    setup auth.js

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,2 @@
+import { handlers } from "@/auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers // Destructuring the handlers object to get the GET and POST functions
