1466fff65a80ef1e6536fe82214f9f8bfe0cf994
ClementMadiot <clementmadiot09@gmail.com>
2025-04-02 15:51:10 +0200

    period resend email

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,11 +2,25 @@ import { auth } from "@/auth";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 import React, { ReactNode } from "react";
+import { after } from "next/server";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 const layout = async ({ children }: { children: ReactNode }) => {
-    const session = await auth();
-  
-    if (!session) redirect("/sign-in");
+  const session = await auth();
+
+  if (!session) redirect("/sign-in");
+
+  // update user's last activity
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session.user.id))
+  });
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
